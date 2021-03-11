0e2515eccbbbbfe0500399e08be7866cabee9457
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2021-03-12 00:43:07 +0800

    Update capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,10 +22,12 @@ set :deploy_to, "/home/deploy/apps"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml"
+# append :linked_files, "config/database.yml"
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system"
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "./bundle", "public/system"
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', './vendor/bundle', 'public/system', 'public/uploads')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
