d699cba66cfd3f92d4213fe365858a4eb3c8f3d0
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2021-03-16 00:09:54 +0800

    Update deploy configuration: - add puma setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,3 +41,43 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+set :puma_user, fetch(:user)
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_control_app, false
+set :puma_default_control_app, "unix://#{shared_path}/tmp/sockets/pumactl.sock"
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 16]
+set :puma_workers, 0
+set :puma_worker_timeout, nil
+set :puma_init_active_record, false
+set :puma_preload_app, false
+set :puma_daemonize, false
+set :puma_plugins, []  #accept array of plugins
+set :puma_tag, fetch(:application)
+set :puma_restart_command, 'bundle exec puma'
+set :puma_service_unit_name, "puma_#{fetch(:application)}_#{fetch(:stage)}"
+set :puma_systemctl_user, :system # accepts :user
+set :puma_enable_lingering, fetch(:puma_systemctl_user) != :system # https://wiki.archlinux.org/index.php/systemd/User#Automatic_start-up_of_systemd_user_instances
+set :puma_lingering_user, fetch(:user)
+set :puma_service_unit_env_file, nil
+set :puma_service_unit_env_vars, []
+
+set :nginx_config_name, "#{fetch(:application)}_#{fetch(:stage)}"
+set :nginx_flags, 'fail_timeout=0'
+set :nginx_http_flags, fetch(:nginx_flags)
+set :nginx_server_name, "localhost #{fetch(:application)}.local"
+set :nginx_sites_available_path, '/etc/nginx/sites-available'
+set :nginx_sites_enabled_path, '/etc/nginx/sites-enabled'
+set :nginx_socket_flags, fetch(:nginx_flags)
+set :nginx_ssl_certificate, "/etc/ssl/certs/#{fetch(:nginx_config_name)}.crt"
+set :nginx_ssl_certificate_key, "/etc/ssl/private/#{fetch(:nginx_config_name)}.key"
+set :nginx_use_ssl, false
+set :nginx_use_http2, true
+set :nginx_downstream_uses_ssl, false
