800ec0bd2ea278c4caf6c8c935b2f4e6d1227f2a
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2021-03-28 00:39:08 +0800

    Set app root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :dummies
+
+  root 'dummies#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
