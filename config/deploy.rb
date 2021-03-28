d1c6243d8b7167785c5738396c20fac26cbaeb95
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2021-03-28 22:45:57 +0800

    Update capistrano deploy configuration: - remove master.key from linked_files - create capistrano tasks to upload master.key

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :assets_roles, []
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/master.key')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "./bundle", "public/system"
@@ -86,3 +86,41 @@ set :nginx_ssl_certificate_key, "/etc/ssl/private/ssl-cert-snakeoil.key"
 set :nginx_use_ssl, false
 set :nginx_use_http2, true
 set :nginx_downstream_uses_ssl, false
+
+
+set :rails_env, 'production'
+
+# Defaults to :db role
+set :migration_role, :app
+
+# Defaults to the primary :db server
+set :migration_servers, -> { primary(fetch(:migration_role)) }
+
+# Defaults to `db:migrate`
+set :migration_command, 'db:migrate'
+
+# Defaults to false
+# Skip migration if files in db/migrate were not modified
+set :conditionally_migrate, true
+
+# Defaults to [:web]
+set :assets_roles, [:web, :app]
+
+# Defaults to 'assets'
+# This should match config.assets.prefix in your rails config/application.rb
+set :assets_prefix, 'prepackaged-assets'
+
+# Defaults to ["/path/to/release_path/public/#{fetch(:assets_prefix)}/.sprockets-manifest*", "/path/to/release_path/public/#{fetch(:assets_prefix)}/manifest*.*"]
+# This should match config.assets.manifest in your rails config/application.rb
+set :assets_manifests, ['app/assets/config/manifest.js']
+
+# RAILS_GROUPS env value for the assets:precompile task. Default to nil.
+set :rails_assets_groups, :assets
+
+# If you need to touch public/images, public/javascripts, and public/stylesheets on each deploy
+set :normalize_asset_timestamps, %w{public/images public/javascripts public/stylesheets}
+
+# Defaults to nil (no asset cleanup is performed)
+# If you use Rails 4+ and you'd like to clean up old assets after each deploy,
+# set this to the number of versions to keep
+set :keep_assets, 2
